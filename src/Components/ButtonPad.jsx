9088d27326b3cc4848f6d05eb2f1f87d71f67558
Phoenix Shane <phoenix.oconnell@gmail.com>
2020-06-03 22:55:25 -0500

    Add additional styling

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -10,13 +10,13 @@ function ButtonPad(props) {
                 <button className="counterButton" onClick={() => props.updateCount(-5)}>-5</button>
                 <button className="counterButton" onClick={() => props.updateCount(-2)}>-2</button>
                 <button className="counterButton" onClick={() => props.updateCount(-1)}>-1</button>
-                <button className="counterButton" onClick={() => props.updateCount(1)}>1</button>
-                <button className="counterButton" onClick={() => props.updateCount(2)}>2</button>
-                <button className="counterButton" onClick={() => props.updateCount(5)}>5</button>
-                <button className="counterButton" onClick={() => props.updateCount(10)}>10</button>
+                <button className="counterButton" onClick={() => props.updateCount(1)}>+1</button>
+                <button className="counterButton" onClick={() => props.updateCount(2)}>+2</button>
+                <button className="counterButton" onClick={() => props.updateCount(5)}>+5</button>
+                <button className="counterButton" onClick={() => props.updateCount(10)}>+10</button>
             </div>
             <div>
-                <button className="resetButton" onClick={props.resetCount}>Reset</button>
+                <button className="counterButton" onClick={props.resetCount}>Reset</button>
             </div>
         </div>
     )
